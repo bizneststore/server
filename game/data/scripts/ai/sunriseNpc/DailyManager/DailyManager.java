d4eb4473fe91b29fec31e4dffa36d840917c1f31
bizneststore <BIZNEST.STORE@GMAIL.COM>
2024-06-21 16:07:23 +0300

    html method for daily

--- game/data/scripts/ai/sunriseNpc/DailyManager/DailyManager.java
+++ game/data/scripts/ai/sunriseNpc/DailyManager/DailyManager.java
@@ -12,9 +12,6 @@ import gr.sr.dailyquests.DailyDBcon;
 
 import ai.npc.AbstractNpcAI;
 
-/**
- * Daily Manager NPC
- */
 public class DailyManager extends AbstractNpcAI
 {
 	private final static int NPC = 576;
@@ -44,11 +41,19 @@ public class DailyManager extends AbstractNpcAI
 		{
 			showDaily(player, npc);
 		}
-		else if (event.startsWith("gatheritems"))
+		else if (event.startsWith("showQuestHtml_"))
 		{
-			return "gather.htm";
+			String questIdStr = event.substring("showQuestHtml_".length());
+			try
+			{
+				int questId = Integer.parseInt(questIdStr);
+				showQuestHtml(player, npc, questId);
+			}
+			catch (NumberFormatException e)
+			{
+				// Handle invalid quest ID
+			}
 		}
-		
 		return event;
 	}
 	
@@ -66,18 +71,16 @@ public class DailyManager extends AbstractNpcAI
 	public void showDaily(L2PcInstance player, L2Npc npc)
 	{
 		StringBuilder tb = new StringBuilder();
-		
 		tb.append("<html><title>Daily Quests</title><body><br>");
 		tb.append("Hello Adventurer, I represent the <font color='LEVEL'>Bounty hunter guild</font>.<br>");
 		tb.append("I am looking for capable people willing to complete a variety of assignments that our clients require, with a reward of course.. <br>");
 		tb.append("Every day at dawn (6 A.M) the guild brings me more assignments, so check every day if you want to keep yourself busy.<br>");
-		tb.append("<br><img src=\"l2ui.squaregray\" width=\"280\" height=\"1s\"><br>");
+		tb.append("<br><img src=\"l2ui.squaregray\" width=\"280\" height=\"1\"><br>");
 		
 		for (Map.Entry<Integer, String> entry : QUESTS.entrySet())
 		{
 			int questId = entry.getKey();
 			String questName = entry.getValue();
-			
 			boolean isQuestCompleted = DailyDBcon.isQuestCompleted(player.getObjectId(), questId);
 			
 			if (isQuestCompleted)
@@ -86,12 +89,38 @@ public class DailyManager extends AbstractNpcAI
 			}
 			else
 			{
-				tb.append("<a action=\"bypass -h Quest DailyManager " + questName.replace(" ", "").toLowerCase() + "\">" + questName + "</a><br>");
+				tb.append("<a action=\"bypass -h Quest DailyManager showQuestHtml_" + questId + "\">" + questName + "</a><br>");
 			}
 		}
 		
+		tb.append("</body></html>");
 		NpcHtmlMessage msg = new NpcHtmlMessage(npc.getObjectId());
 		msg.setHtml(tb.toString());
 		player.sendPacket(msg);
+	}
+	
+	private void showQuestHtml(L2PcInstance player, L2Npc npc, int questId)
+	{
+		String htmlFile = getQuestHtmlFile(questId);
+		if (htmlFile != null)
+		{
+			NpcHtmlMessage html = new NpcHtmlMessage(npc.getObjectId());
+			html.setHtml(htmlFile);
+			player.sendPacket(html);
+		}
+	}
+	
+	private String getQuestHtmlFile(int questId)
+	{
+		switch (questId)
+		{
+			case 11000:
+				return "data/scripts/quests/Q11000_GatherItems/gather.htm";
+			// Add cases for other quests here
+			// case 11001:
+			// return "data/scripts/quests/Q11001_AnotherQuest/anotherquest.htm";
+			default:
+				return null; // Handle unknown quests if needed
+		}
 	}
 }
